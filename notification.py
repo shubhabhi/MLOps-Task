074966cc58a72be0948ce7ff22686196dbfb06f2
Shubham Abhishek <43480294+shubhabhi@users.noreply.github.com>
2020-05-30 16:48:08 +0530

    first

---
+++ notification.py
@@ -0,0 +1,34 @@
+f=open("/mlopsdata/Accuracy.txt","r")
+y=[]
+file=f.readlines()
+for line in file:
+    y.append(float(line.strip()))
+accuracy=str(y[0])
+f.close()
+print("accurcacy for your model:",accuracy)
+
+
+import smtplib, ssl
+
+port = 465 #For SSL
+smtp_server ="smtp.gmail.com"
+sender_email="shubhabhi40@gmail.com"    #Sender's Mail Address
+receiver_email="abhishekshubham360@gmail.com"      #Receiver's Mail Address
+password="xecbeupbulzfwpos"
+if accuracy > "90":
+    message="""    Subject: Report | Prediction Program
+    
+    CONGRATULATIONS! 
+    Your code achieved {}% accuracy.""".format(accuracy)
+else:
+    message="""    Subject: Report | Prediction Program
+    
+    Train Again!
+    Your code got {}% accuracy.""".format(accuracy)
+    
+context=ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email,password)
+    server.sendmail(sender_email, receiver_email, message)
+    
+    
